0dd332dd039949520de38594c208e14ba7b4edf1
rishi-raj19 <81846694+rishi-raj19@users.noreply.github.com>
2023-07-23 21:40:56 +0530

    Delete InstagramBackend-master directory

--- InstagramBackend-master/src/main/java/com/example/InstagramBackend/repository/IUserRepo.java
+++
@@ -1,8 +0,0 @@
-package com.example.InstagramBackend.repository;
-
-import com.example.InstagramBackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface IUserRepo extends JpaRepository<User,Long> {
-    User findFirstByUserEmail(String email);
-}
